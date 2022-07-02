15d5a31e9e77ea6445dce75861ff439abbd0df50
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-07-01 20:32:44 -0400

    finished the today weather layout

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 var searchBar = document.getElementById('search-bar')
-
+var searchButton = document.getElementById('search-button')
+var todayTitle = document.getElementById('today-title')
+var todayTemp = document.getElementById('today-temp')
+var todayWind = document.getElementById('today-wind')
+var todayHumidity = document.getElementById('today-humidity')
+var UVIndex = document.getElementById('UV-Index')
 var url = 'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=07fbd3ab5870b3d955e106314afd4001'
+var todayContainer = document.getElementById('today-container')
+var icon = document.getElementById('icon')
 
+
+searchButton.addEventListener("click", 
+function editSearch(e) {
+    e.preventDefault()
+    todayContainer.setAttribute("style", "border: 2px solid black;")
+
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&APPID=07fbd3ab5870b3d955e106314afd4001&units=imperial`
 fetch(url, {cache: "reload"})
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
+    var iconcode = data.weather[0].icon;
+    console.log(data.weather[0].icon)
+    var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png"
+    console.log(iconurl)
+    icon.setAttribute('src', `https://openweathermap.org/img/w/${iconcode}.png`)
+    todayTitle.textContent = searchBar.value
+    todayTemp.textContent = "Temperature is: " + data.main.temp + "F"
+    todayWind.textContent = "The wind speed is: " + data.wind.speed
+    todayHumidity.textContent = "The humidity is: " + data.main.humidity
     console.log(data);
-  });
+    console.log(data.weather)
+
+  })});
